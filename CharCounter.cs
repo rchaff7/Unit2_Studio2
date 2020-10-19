e25a4d6a318fe576445d84d601cdcf57da320f9d
Ryan Chaffee <rchaff7@yahoo.com>
2020-10-18 23:39:10 -0500

    Add project files.

---
+++ CharCounter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studio2
+{
+    class CharCounter
+    {
+        public static void CountInput(string input)
+        {
+            char[] charArr = input.ToLower().ToCharArray();
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+            
+            foreach(char c in charArr)
+            {
+                if (Char.IsLetter(c))
+                {
+                    if (counts.ContainsKey(c))
+                    {
+                        counts[c]++;
+                    } 
+                    else
+                    {
+                        counts.Add(c, 1);
+                    }
+                }
+            }
+
+            foreach(KeyValuePair<char, int> pair in counts)
+            {
+                Console.WriteLine(pair.Key + ": " + pair.Value);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Studio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = System.IO.File.ReadAllText("TextFile1.txt");
+
+            // Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.
+
+            Console.WriteLine("Reading input from local text file:\n");
+            CharCounter.CountInput(input);
+        }
+    }
+}
+
+//3.8.5.Bonus Missions
+//Try these modifications on your code:
+//    Prompt the user to enter the string in the command line.
+//    Make the character counts case-insensitive.
+//    Exclude non-alphabetic characters.
+
+//3.8.5.1. Super Bonus
+//Read the string in from a file.
